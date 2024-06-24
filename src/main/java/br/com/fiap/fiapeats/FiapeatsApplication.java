154f6c4ee1588c5da23eb3eccc24cbf086ebbc7d
Dilan Lima <dilanlima@Dilans-MacBook-Air.local>
2024-06-24 18:48:00 -0300

    feature: initial commit of project

---
+++ src/main/java/br/com/fiap/fiapeats/FiapeatsApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.fiapeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiapeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiapeatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/fiapeats/FiapeatsApplicationTests.java
@@ -0,0 +1,11 @@
+package br.com.fiap.fiapeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapeatsApplicationTests {
+
+
+
+}
