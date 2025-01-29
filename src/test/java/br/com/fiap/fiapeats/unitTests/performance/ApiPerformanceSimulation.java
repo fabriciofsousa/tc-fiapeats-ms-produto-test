b1bcd50435f56c9852ffc99b46e551819c515e63
fabriciofsousa <134175378+fabriciofsousa@users.noreply.github.com>
2025-01-29 01:17:41 -0300

    finalizando teste e adicionando no github actions

--- src/test/java/br/com/fiap/fiapeats/unitTests/performance/ApiPerformanceSimulation.java
+++ src/test/java/br/com/fiap/fiapeats/unitTests/performance/ApiPerformanceSimulation.java
@@ -19,18 +19,9 @@ public class ApiPerformanceSimulation extends Simulation {
 
     ActionBuilder criarProdutoRequest = http("Criar Produto")
             .post("/fiapeats/produto")
-            .body(StringBody("{ \"nome\": \"Refrigerante\", \"descricao\": \"Lata 350ml\", \"preco\": 5.99, \"categoria\": \"Bebida\", \"imagemUrl\": \"http://imagem.com/img.png\" }"))
+            .body(StringBody("{ \"nome\": \"Refrigerante\", \"descricao\": \"Lata 350ml\", \"valor\": 5.99, \"categoria\": \"Bebida\", \"imagemUrl\": \"http://imagem.com/img.png\" }"))
             .check(status().is(201))
             .check(jsonPath("$.id").saveAs("produtoId"));
-
-    ActionBuilder editarProdutoRequest = http("Editar Produto")
-            .put("/fiapeats/produto/#{produtoId}")
-            .body(StringBody("{ \"nome\": \"Refrigerante Atualizado\", \"descricao\": \"Lata 500ml\", \"preco\": 6.99, \"categoria\": \"Bebida\", \"imagemUrl\": \"http://imagem.com/updated.png\" }"))
-            .check(status().is(200));
-
-    ActionBuilder removerProdutoRequest = http("Remover Produto")
-            .delete("/fiapeats/produto/#{produtoId}")
-            .check(status().is(204));
 
     ActionBuilder listarProdutosRequest = http("Listar Produtos")
             .get("/fiapeats/produto")
@@ -38,14 +29,6 @@ public class ApiPerformanceSimulation extends Simulation {
 
     ScenarioBuilder cenarioCriarProduto = scenario("Criar Produto")
             .exec(criarProdutoRequest);
-
-    ScenarioBuilder cenarioEditarProduto = scenario("Editar Produto")
-            .exec(criarProdutoRequest)
-            .exec(editarProdutoRequest);
-
-    ScenarioBuilder cenarioRemoverProduto = scenario("Remover Produto")
-            .exec(criarProdutoRequest)
-            .exec(removerProdutoRequest);
 
     ScenarioBuilder cenarioListarProdutos = scenario("Listar Produtos")
             .exec(listarProdutosRequest);
@@ -56,19 +39,11 @@ public class ApiPerformanceSimulation extends Simulation {
                         rampUsersPerSec(1).to(10).during(Duration.ofSeconds(10)),
                         constantUsersPerSec(10).during(Duration.ofSeconds(60)),
                         rampUsersPerSec(10).to(1).during(Duration.ofSeconds(10))),
-                cenarioEditarProduto.injectOpen(
-                        rampUsersPerSec(1).to(10).during(Duration.ofSeconds(10)),
-                        constantUsersPerSec(10).during(Duration.ofSeconds(60)),
-                        rampUsersPerSec(10).to(1).during(Duration.ofSeconds(10))),
-                cenarioRemoverProduto.injectOpen(
-                        rampUsersPerSec(1).to(10).during(Duration.ofSeconds(10)),
-                        constantUsersPerSec(10).during(Duration.ofSeconds(60)),
-                        rampUsersPerSec(10).to(1).during(Duration.ofSeconds(10))),
-                cenarioListarProdutos.injectOpen(
-                        rampUsersPerSec(1).to(100).during(Duration.ofSeconds(10)),
-                        constantUsersPerSec(100).during(Duration.ofSeconds(60)),
-                        rampUsersPerSec(100).to(1).during(Duration.ofSeconds(10))))
-                .protocols(httpProtocol)
+                        cenarioListarProdutos.injectOpen(
+                                rampUsersPerSec(1).to(100).during(Duration.ofSeconds(10)),
+                                constantUsersPerSec(100).during(Duration.ofSeconds(60)),
+                                rampUsersPerSec(100).to(1).during(Duration.ofSeconds(10)))
+                ).protocols(httpProtocol)
                 .assertions(
                         global().responseTime().max().lt(50),
                         global().failedRequests().count().is(0L));
