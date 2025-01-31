84e1b296993f99ec3a3d85b8d64ab3e74c36d16f
fabriciofsousa <134175378+fabriciofsousa@users.noreply.github.com>
2025-01-31 20:19:20 -0300

    finalizando teste e adicionando no github actions

--- src/test/java/br/com/fiap/fiapeats/unitTests/bdd/RunCucumberTest.java
+++ src/test/java/br/com/fiap/fiapeats/unitTests/bdd/RunCucumberTest.java
@@ -2,12 +2,17 @@ package br.com.fiap.fiapeats.unitTests.bdd;
 
 import org.junit.platform.suite.api.*;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import io.cucumber.spring.CucumberContextConfiguration;
+
 
 import static io.cucumber.core.options.Constants.GLUE_PROPERTY_NAME;
 
 @Suite  // NOSONAR
+@CucumberContextConfiguration
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@SelectClasspathResource("features/produto.feature")
+@TestPropertySource(locations = "classpath:application-test.properties")
+@SelectClasspathResource("features")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "br.com.fiap.fiapeats.unitTests.bdd")
 public class RunCucumberTest {
 }
